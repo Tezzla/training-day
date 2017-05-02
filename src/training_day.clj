b2e0e49f3eb56f8460bf912880c0ef2d43498a5a
Tezzla <titta.sch@gmail.com>
2017-05-02 13:03:51 +0300

    Training day finished

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,30 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42ß)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn square [x] (* x x))
 
-(defn average [a b]
-  ":(")
+(square 2)
+
+(defn average [a b] (/ (+ a b) 2))
+
+(average 2 4)
+
+(+ 4 (* 2 3))
+
+(+ 3 4 5 6)
+
+((fn [who] (str "Hello, " who "!")) "Jani")
+
+(fn [name] (str "Welcome to Rivendell mr " name))
+
+(def hello (fn [who] (str "Hello, " who "!")))
+(hello "beautiful")
+
+(defn                                 ; Start a function definition:
+  hello                               ; name
+  "Gives out personalized greetings." ; a optional docstring
+  [who]                               ; parameters inside brackets
+  (str "Hello, " who "!"))
